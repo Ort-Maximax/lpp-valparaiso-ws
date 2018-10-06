24693cd8d46a040d1c8ee56ec435f38dacabfcde
Edhrendal <bvillena.git@laposte.net>
2018-10-06 15:30:32 +0200

    ADD Gestion fichier configuration
    * Récupère les informations de la configuration pour l'application.

--- app.py
+++ app.py
@@ -1,18 +1,45 @@
 #!/usr/bin/python3
 
+import configparser
+from pathlib import Path
+
 from flask import Flask
 from flask import request
 
 ########## CONST ##########
-APP_CONF = 'app_flask'
 APP_CONF_HOST = 'HOST'
 APP_CONF_PORT = 'PORT'
+APP_CONF_SECTION = 'app_flask'
+CONF_FILE = 'app.ini'
+
+FLASK_DEFAULT_HOST = 'localhost'
+FLASK_DEFAULT_PORT = 5000
 
 JSON_METHOD = 'method'
 JSON_PATH = 'path'
 
 
 ########## CONFIG ##########
+def checkConfig(config):
+	confFile = Path(CONF_FILE)
+	if confFile.is_file():
+		config.read(CONF_FILE)
+		return True
+	return False
+
+def getConfParamFromSection(config, conf_section, conf_param):
+	conf_sections = config.sections()
+	if conf_section in conf_sections and conf_param in config[conf_section]:
+		return config[conf_section][conf_param]
+	return False
+
+##### APP CONFIG
+config = configparser.ConfigParser()
+app_host = FLASK_DEFAULT_HOST
+app_port = FLASK_DEFAULT_PORT
+if checkConfig(config):
+	app_host = getConfParamFromSection(config, APP_CONF_SECTION, APP_CONF_HOST) if getConfParamFromSection(config, APP_CONF_SECTION, APP_CONF_HOST) else FLASK_DEFAULT_HOST
+	app_port = int(getConfParamFromSection(config, APP_CONF_SECTION, APP_CONF_PORT)) if getConfParamFromSection(config, APP_CONF_SECTION, APP_CONF_PORT) else FLASK_DEFAULT_PORT
 
 
 
@@ -41,4 +68,4 @@ def ffmpegAction():
 	return checkJSON(request.get_json())		
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(debug=True, host=app_host, port=app_port)
--- demo.sh
+++ demo.sh
@@ -1,3 +1,5 @@
+# GET Test
+curl --request GET http://localhost:5000/
 
 # POST JSON
 curl --header "Content-Type: application/json" --request POST --data '{"method": "hflip", "path":"/data/demo/image.png"}' http://localhost:5000/ffmpeg
